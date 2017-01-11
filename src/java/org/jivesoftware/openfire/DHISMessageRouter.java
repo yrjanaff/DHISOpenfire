71c3530586bf132bc8c2f5d9968d269a7fa52622
Yrjan A. F. Fraschetti <yrjanaff@1x-193-157-200-122.uio.no>
2017-01-11 17:18:17 +0100

    Added location log

--- src/java/org/jivesoftware/openfire/DHISMessageRouter.java
+++ src/java/org/jivesoftware/openfire/DHISMessageRouter.java
@@ -120,7 +120,7 @@ public class DHISMessageRouter
         String dhisURL = "https://yj-dev.dhis2.org/dhis/api/";
         String authStr = username + ":" + password;
         String authEncoded = Base64.encodeBytes( authStr.getBytes() );
-        Sring location = "";
+        String location = "";
         int code = -1;
         String body = "";
         HttpResponseObject hro = null;
