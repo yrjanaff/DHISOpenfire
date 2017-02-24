de27915589f1422edbb26cc2600293d9418a2b34
Yrjan Fraschetti <yrjanaff@gmail.com>
2017-02-24 10:11:24 +0200

    fixed derp

--- src/java/org/jivesoftware/openfire/DHISMessageRouter.java
+++ src/java/org/jivesoftware/openfire/DHISMessageRouter.java
@@ -318,7 +318,7 @@ public class DHISMessageRouter
 
     private HttpResponseObject dhisHttpRequest( String urlE, String username, String password, String requestMethod, String jsonBody )
     {
-        String dhisURL = "https://yj-dev.dhis2.org/dhis/api/me";
+        String dhisURL = "https://yj-dev.dhis2.org/dhis/api/";
         String authStr = username + ":" + password;
         String authEncoded = Base64.encodeBytes( authStr.getBytes() );
         String location = "";
